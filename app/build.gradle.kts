cd6b91c5c68be8059bc89e0ad5d98437d755475b
Kyuu Stash <kyuustash@gmail.com>
2026-07-11 12:19:10 +0800

    chore: release v0.0.32

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kyuusanq3.mixauto"
         minSdk = 24
         targetSdk = 34
-        versionCode = 31
-        versionName = "0.0.31"
+        versionCode = 32
+        versionName = "0.0.32"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
